3718f6c16770a9328187f5fcac3ad24d711db4c1
Jidraf Kimachia <raphjid170@gmail.com>
2022-10-28 11:02:12 +0300

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    def match(possible_anagram_array)
+        possible_anagram_array.filter do |element|
+            element.chars.sort == word.chars.sort
+        end
+    end
+    
+end
+
+testing = Anagram.new("Angel")
+testing.match(["gelan", "glean", "lenag"])
